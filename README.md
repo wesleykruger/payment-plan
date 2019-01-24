f8cdcd57cb264424abc504de0786b71300301c53
wkruger-work <wesleykruger@gmail.com>
2019-01-24 13:41:15 -0600

    restructured project and fixed remainingbal on numPayments

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
-# PplnApp
+# PplnApp-Part1
 
 ## Getting Started
+
 All you need to do is to clone this repository,
 
 Run npm start and the app will be available on port 5000.
 
 ## Purpose
+
 This is a demo of a payment plan calculator that can be constructed in AngularJS.
 
 ### Uses
---
+++ js/controllers.js
@@ -0,0 +1,71 @@
+angular.module('PplnApp.controllers', [])
+
+  /* Payment plan controller */
+  .controller('payPlanController', function ($scope, paymentCalculatorService, moment) {
+    $scope.paymentArr = [];
+    $scope.installmentAmount = 0;
+
+    $scope.doCalculate = function (stringOwedAmount, stringDownPayment, numPayments, firstDate, frequency, calcType, stringInstallmentAmount) {
+      let installmentArray = [];
+      let owedAmount = parseFloat(stringOwedAmount);
+      let downPayment = parseFloat(stringDownPayment);
+      let installmentAmount = parseFloat(stringInstallmentAmount);
+      let dateForThisIndex = moment(firstDate).format("MM/DD/YYYY");
+      let remainingBal = owedAmount - downPayment;
+      if (calcType === 'installmentAmount') {
+        while (remainingBal > 0) {
+          // remainingBal += interestRate;
+          if (remainingBal >= installmentAmount) {
+            remainingBal -= installmentAmount;
+            installmentArray.push( {
+              installmentAmount: installmentAmount,
+              date: dateForThisIndex,
+              remainingBal: remainingBal
+            });
+            if (frequency === 'weekly') {
+              dateForThisIndex = moment(moment(dateForThisIndex).add(1, 'w')).format("MM/DD/YYYY");
+            } else {
+              dateForThisIndex = moment(moment(dateForThisIndex).add(1, 'M')).format("MM/DD/YYYY");
+            }
+          } else {
+            let finalPayment = remainingBal;
+            installmentArray.push({
+              installmentAmount: finalPayment,
+              date: dateForThisIndex,
+              remainingBal: 0
+            });
+            remainingBal = 0;
+          }
+        }
+      } else if (calcType === 'numInstallments') {
+        let newOwedAmount = owedAmount - downPayment;
+        let installmentAmount = 0;
+        // This is not accurate enough for production. This truncate decimals to match currency format, so a straight divide is not accurate enough.
+        // There is also interest to consider
+        if (newOwedAmount > 0) {
+          installmentAmount = newOwedAmount / numPayments
+        }
+
+        let remainingBal = newOwedAmount
+        for (let i = 0; i < numPayments; i++) {
+          remainingBal -= installmentAmount;
+          installmentArray.push({
+            installmentAmount: installmentAmount,
+            date: dateForThisIndex,
+            remainingBal: 0
+          })
+
+          if (frequency === 'weekly') {
+            console.log(dateForThisIndex)
+            dateForThisIndex = moment(moment(dateForThisIndex).add(1, 'w')).format("MM/DD/YYYY");
+          } else {
+            dateForThisIndex = moment(moment(dateForThisIndex).add(1, 'M')).format("MM/DD/YYYY");
+          }
+
+        }
+      }
+      $scope.paymentList = installmentArray
+    };
+  })
+
+
